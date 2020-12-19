ebd4adcdd8163b4bacf26e4633ceade875e009cc
Sivaram Prasad <35053169+sivaramdraksharam@users.noreply.github.com>
2020-12-19 18:41:12 +0530

    javascript day 1
    javasrcipt training using javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+    //comment
+    console.log('Helloworld');
+    let name = 'Siva';
+    console.log(name);  
+    //Variable name can not be a reserved word.
+    //it should be meaningful - do not give strange names like x
+    //it can't start with a number
+    //it can't contain space or hyphen
+
+    let firstName = 'Sivaram';
+    let lastName='Prasad';
+
+    let interestRate = 0.3;
+
+    const pi = 3.141;
+   // pi = 34;
+   // a constant variable can not be assigned any value
+
+   
+
+
